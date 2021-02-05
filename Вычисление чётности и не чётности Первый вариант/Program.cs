e6c278af767969be698b9d35e8f943d0f3ca7f6c
Bitard-art <78072008+Bitard-art@users.noreply.github.com>
2021-02-05 16:25:04 +0400

    Add files via upload

---
+++ Вычисление чётности и не чётности Первый вариант/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Вычисление_чётности_и_не_чётности_Первый_вариант
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = int.Parse(Console.ReadLine());
+
+            while (a < b)
+            {
+                a++;
+                Console.WriteLine(a);
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("Чётное число");
+
+                }
+                else
+                {
+                    Console.WriteLine("Не чётное число");
+                }
+            }
+
+
+        }
+    }
+}
