37b355d0e1fa83e0a1b2f8d86a15cfb1c00f7de2
Bitard-art <78072008+Bitard-art@users.noreply.github.com>
2021-02-05 16:53:56 +0400

    Add files via upload

---
+++ Счётчик чётных и не чётных Второй вариант/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Счётчик_чётных_и_не_чётных_Второй_вариант
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = int.Parse(Console.ReadLine());
+            uint Even = 0;
+            uint Noeven = 0;
+
+
+            while (a < b)
+            {
+                a++;
+                Console.WriteLine(a);
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("Чётное число");
+                    Even++;
+                    Console.WriteLine("Кол-во Чёртных" + "  " + Even);
+
+                }
+                else
+                {
+                    Console.WriteLine("Не чётное число");
+                    Noeven++;
+                    Console.WriteLine("Кол-во не Чётных" + "  " + Noeven);
+
+
+                }
+            }
+
+
+        }
+    }
+}
