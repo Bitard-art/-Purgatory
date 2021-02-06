745ef621c434dc7b73b5af2bb64cc8b8594af787
Bitard-art <78072008+Bitard-art@users.noreply.github.com>
2021-02-06 17:48:49 +0400

    Add files via upload

---
+++ Треугольники/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Треугольники
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину");
+            int Length = int.Parse(Console.ReadLine());
+
+
+            for (int a = 0; a < Length; a++)
+            {
+                for (int b = 0; b <= a; b++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            for (int a = 0; a < Length; a++)
+            {
+                for (int b = Length; b > a; b--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            for (int a = 0; a < Length; a++)
+            {
+                for (int b = Length; b > a; b--)
+                {
+                    Console.Write(" ");
+                }
+                for (int b = 0; b <= a; b++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            for (int a = 0; a < Length; a++)
+            {
+                for (int b = 0; b <= a; b++)
+                {
+                    Console.Write(" ");
+                }
+                for (int b = Length; b > a; b--)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
