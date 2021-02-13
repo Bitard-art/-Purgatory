e002d77fae740ebb0d92e33b701cf0118b886fc4
Bitard-art <78072008+Bitard-art@users.noreply.github.com>
2021-02-13 13:54:36 +0400

    Add files via upload

---
+++ ДЗ №7.1 Видео №46/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ДЗ__7._1_Видео__46
+{
+    class Program
+    {
+        
+            static int Keu(int[] masiv, int ox)
+            {
+                for (int i = 0; i < masiv.Length; i++)
+                {
+                    if (masiv[i] == ox)
+                    {
+                        return i;
+                    }
+
+                }
+
+                return -1;
+            }
+            static void Main(string[] args)
+            {
+                int[] dog = { 3, 1, 5, 26, 1 };
+                int cat = Keu(dog, 26);
+
+                Console.WriteLine(cat);
+                Console.ReadLine();
+            }
+        }
+
+    }
