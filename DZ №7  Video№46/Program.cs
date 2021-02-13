7b0b3a9825b6c95ccd3e27588dcbb608e5c91d7b
Bitard-art <78072008+Bitard-art@users.noreply.github.com>
2021-02-13 13:45:19 +0400

    Add files via upload

---
+++ DZ №7  Video№46/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DZ__7__Video_46
+{
+    class Program
+    {
+        static void WoW(string ARK, uint Surv)
+        {
+            for (int i = 0; i < Surv; i++)
+            {
+                Console.WriteLine(ARK);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите желаемый символ");
+            String ARK = Console.ReadLine();
+
+            Console.WriteLine("Желаемое количество символов");
+            uint Surv = uint.Parse(Console.ReadLine());
+
+            Program.WoW(ARK, Surv);
+            Console.ReadLine();
+
+        }
+
+
+
+
+    }
+}
